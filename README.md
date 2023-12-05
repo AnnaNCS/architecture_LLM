2ca34abb9cfef1d5c7f9ae4d5de6fb8f30a62b67
AnnaNCS <an3143@columbia.edu>
2023-12-05 00:47:59 +0000

    part 2 and 3 done

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # architecture_LLM
 
+
+Part 2:
 To run the code, use the following command:
 gcc softmax.c -o softmax -lm
 gcc softmax_opt.c -o softmax_opt -lm -fopenmp
 gcc -Ofast softmax_opt.c -o softmax_opt -lm -fopenmp
 
+
 ./softmax 
 ./softmax_opt
+
+Part 3: 
+
+To run the code, use the following command:
+gcc lookups.c -o lookups
+gcc -O3 lookups_opt.c -o lookups_opt -fopenmp
+
+Additional: 
+gcc -O3 -fprofile-generate lookups_opt.c -o lookups_opt -fopenmp
+./lookups_opt
+gcc -O3 -fprofile-use lookups_opt.c -o lookups_opt -fopenmp
+
+./lookups 
+./lookups_opt
--- lookups_opt.c
+++ lookups_opt.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <immintrin.h>
 #include <stdint.h> 
+#include <omp.h>
+#include <string.h>
 
 #define EMBEDDING_SIZE 1024
 #define VOCAB_SIZE 50000
@@ -12,12 +14,26 @@ static inline uint64_t rdtsc() {
     return a | ((uint64_t)d << 32);
 }
 
-// Function to perform embedding lookup
+
 void embedding_lookup(double** embedding_matrix, int* indices, double** result, int num_indices) {
     for (int i = 0; i < num_indices; ++i) {
-		for (int j = 0; j < EMBEDDING_SIZE; ++j) {
-			result[i][j] = embedding_matrix[indices[i]][j];
-        } 
+        double* src_row = embedding_matrix[indices[i]];
+        double* dest_row = result[i];
+        int j, h;
+
+        #pragma omp simd
+        for (j = 0; j < EMBEDDING_SIZE - 4; j += 4) {
+            _mm_prefetch((const char*)&src_row[j + 64], _MM_HINT_T0);
+            dest_row[j] = src_row[j];
+            dest_row[j + 1] = src_row[j + 1];
+            dest_row[j + 2] = src_row[j + 2];
+            dest_row[j + 3] = src_row[j + 3];
+        }
+
+        // Handle the remaining elements (if any)
+        for (h = j; h < EMBEDDING_SIZE; ++h) {
+            dest_row[j] = src_row[j];
+        }
     }
 }
 
